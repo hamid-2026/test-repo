2116c91a2e2a5cdfb93e5113ce3b74024be9b4a9
= <hamidjaffari21@gmail.com>
2026-04-17 22:12:38 +0430

    add Hello World label

--- App.java
+++ App.java
@@ -7,6 +7,11 @@ public class App extends JFrame {
         setSize(500, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
+
+        JLabel lb = new JLabel("Hello World");
+        lb.setFont(new Font("Arial",Font.PLAIN,18));
+        add(lb);
 
     }
     public static void main(String[] args){
